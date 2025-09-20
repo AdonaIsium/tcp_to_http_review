8509ac0b6f4adbd2770f9433f1dbeb85bdd4aa3a
Lanre <lanre.arisen@gmail.com>
2025-09-20 15:52:20 -0500

    body parsing complete phew

--- internal/request/request.go
+++ internal/request/request.go
@@ -4,14 +4,19 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strconv"
+
+	"github.com/AdonaIsium/tcp_to_http_review/internal/headers"
 )
 
 type parserState string
 
 const (
-	StateInit  parserState = "init"
-	StateDone  parserState = "done"
-	StateError parserState = "error"
+	StateInit    parserState = "init"
+	StateDone    parserState = "done"
+	StateBody    parserState = "body"
+	StateHeaders parserState = "headers"
+	StateError   parserState = "error"
 )
 
 type RequestLine struct {
@@ -22,12 +27,29 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	Headers     *headers.Headers
 	state       parserState
+	Body        string
+}
+
+func getInt(headers *headers.Headers, name string, defaultValue int) int {
+	valueStr, exists := headers.Get(name)
+	if !exists {
+		return defaultValue
+	}
+
+	value, err := strconv.Atoi(valueStr)
+	if err != nil {
+		return defaultValue
+	}
+	return value
 }
 
 func newRequest() *Request {
 	return &Request{
-		state: StateInit,
+		state:   StateInit,
+		Headers: headers.NewHeaders(),
+		Body:    "",
 	}
 }
 
@@ -43,6 +65,7 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	buf := make([]byte, 1024)
 	bufLen := 0
 	for !request.done() {
+
 		n, err := reader.Read(buf[bufLen:])
 		// TODO: what to do here?
 		if err != nil {
@@ -52,6 +75,7 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		bufLen += n
 
 		readN, err := request.parse(buf[:bufLen])
+
 		if err != nil {
 			return nil, err
 		}
@@ -59,11 +83,16 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		copy(buf, buf[readN:bufLen])
 		bufLen -= readN
 
+		if err == io.EOF && readN == 0 {
+			break
+		}
+
 	}
 	return request, nil
 }
 
 func parseRequestLine(b []byte) (*RequestLine, int, error) {
+
 	idx := bytes.Index(b, CRLF)
 	if idx == -1 {
 		return nil, 0, nil
@@ -86,16 +115,24 @@ func parseRequestLine(b []byte) (*RequestLine, int, error) {
 	return rl, read, nil
 }
 
+func (r *Request) hasBody() bool {
+	// TODO: when doing chunked encoding, update this method
+	length := getInt(r.Headers, "content-length", 0)
+	return length > 0
+}
+
 func (r *Request) parse(data []byte) (int, error) {
 	read := 0
 
 outer:
 	for {
+		currentData := data[read:]
+
 		switch r.state {
 		case StateError:
 			return 0, ErrorRequestInErrorState
 		case StateInit:
-			rl, n, err := parseRequestLine(data[read:])
+			rl, n, err := parseRequestLine(currentData)
 			if err != nil {
 				r.state = StateError
 				return 0, err
@@ -105,14 +142,52 @@ outer:
 			}
 			r.RequestLine = *rl
 			read += n
+			r.state = StateHeaders
+		case StateHeaders:
+			n, done, err := r.Headers.Parse(currentData)
+			if err != nil {
+				r.state = StateError
+				return 0, err
+			}
 
-			r.state = StateDone
+			if n == 0 {
+				break outer
+			}
+
+			read += n
+
+			if done {
+				if r.hasBody() {
+					r.state = StateBody
+				} else {
+					r.state = StateDone
+				}
+			}
+		case StateBody:
+			length := getInt(r.Headers, "content-length", 0)
+			if length == 0 {
+				panic("chunked not implemented")
+			}
+
+			remaining := min(length-len(r.Body), len(currentData))
+
+			if remaining == 0 {
+				break outer
+			}
+
+			r.Body += string(currentData[:remaining])
+			read += remaining
+
+			if len(r.Body) == length {
+				r.state = StateDone
+			}
 		case StateDone:
 			break outer
+		default:
+			panic("somehow we have programmed poorly")
 		}
 	}
 	return read, nil
-
 }
 
 func (r *Request) done() bool {
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -24,10 +24,10 @@ func (cr *chunkReader) Read(p []byte) (n int, err error) {
 
 	n = copy(p, cr.data[cr.pos:endIndex])
 	cr.pos += n
-	if n > cr.numBytesPerRead {
-		n = cr.numBytesPerRead
-		cr.pos -= n - cr.numBytesPerRead
-	}
+	// if n > cr.numBytesPerRead {
+	// 	n = cr.numBytesPerRead
+	// 	cr.pos -= n - cr.numBytesPerRead
+	// }
 	return n, nil
 }
 
@@ -54,5 +54,64 @@ func TestRequestLineParse(t *testing.T) {
 
 	// Test: Invalid number of parts in request line
 	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestParseHeaders(t *testing.T) {
+	// Test: Standard Headers
+	reader := &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	host, ok := r.Headers.Get("host")
+	assert.True(t, ok)
+	assert.Equal(t, "localhost:42069", host)
+
+	useragent, ok := r.Headers.Get("user-agent")
+	assert.True(t, ok)
+	assert.Equal(t, "curl/7.81.0", useragent)
+
+	accept, ok := r.Headers.Get("accept")
+	assert.True(t, ok)
+	assert.Equal(t, "*/*", accept)
+
+	// Test: Malformed Header
+	reader = &chunkReader{
+		data:            "GET / HTTP/1.1\r\nHost localhost:42069\r\n\r\n",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
+	require.Error(t, err)
+}
+
+func TestParseBody(t *testing.T) {
+	// Test: Standard Body
+	reader := &chunkReader{
+		data: "POST /submit HTTP/1.1\r\n" +
+			"Host: localhost:42069\r\n" +
+			"Content-Length: 13\r\n" +
+			"\r\n" +
+			"hello world!\n",
+		numBytesPerRead: 3,
+	}
+	r, err := RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "hello world!\n", string(r.Body))
+
+	// Test: Body shorter than reported content length
+	reader = &chunkReader{
+		data: "POST /submit HTTP/1.1\r\n" +
+			"Host: localhost:42069\r\n" +
+			"Content-Length: 20\r\n" +
+			"\r\n" +
+			"partial content",
+		numBytesPerRead: 3,
+	}
+	r, err = RequestFromReader(reader)
 	require.Error(t, err)
 }
